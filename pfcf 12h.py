1fed675feffd98655be12549e7611168b4bca2e6
rafaelscherer3 <rafaelscherer3@gmail.com>
2026-01-19 18:14:43 -0300

    Add files via upload

---
+++ pfcf 12h.py
@@ -0,0 +1,65 @@
+#import math 
+#diametro = float(input('diametro:'))
+#raio = diametro / 2
+#area = 2 * raio * math.pi
+#print (f'area = {round(area, 1)}')
+
+#import math
+#a=float (input('base'))
+#b= float (input('altura'))
+#if b == float('0'):
+#    print ('fim')
+#else:
+#    hipoten = math.sqrt(a**2+b**2)
+#
+#print (f'a hipotenusa é {round(hipoten,1)} ')
+
+#a = float(input('primeiro numero: '))
+#b = float(input('segundo numero:'))
+#op = input('sinal')
+#if op == '+':
+#    result = a + b
+#elif op == '-':
+#    result = a - b
+#elif op == '/':
+#    result = a / b
+#elif op == '*':
+#    result = a * b#
+
+ #   print (round(result,2))
+#lse: 
+#   print (f' operacao {op} não valida')
+
+#peso = float(input('qual o seu peso? '))
+#unidade = input('kg ou lb?')
+#if unidade == 'kg': 
+#    peso = peso * 2
+#    un = 'lbs'
+#    print (f'seu peso é: {peso} {un}')
+#elif unidade == 'lb':
+#    peso = peso / 2
+#    un = 'kg'
+#    print (f'seu peso é: {peso} {un}')
+
+#else: 
+#    print (f' {unidade} é uma unidade desconhecida')
+#num = float(input ('numero: '))
+#print ('positivo' if num > 0 else 'negativo')
+#if num > 0:
+#    print ('positivo')
+#else:
+#    print ('negativo')
+#print ('par' if num % 2 == 0 else 'impar')
+nome = input('nome: ')
+#resultado = len(nome)
+#if resultado > 5:
+#    print('2avfjdknvjkd')
+#else:
+#    print (resultado)
+resultado = nome.find('a')
+#print ('njfdnvhjfnbhjfg' if resultado > 5 else 'menor')
+
+print (resultado)
+
+
+ 
