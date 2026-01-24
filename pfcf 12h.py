06b9314acb15d67a684c5c3c4afe9dbce5826005
rafaelscherer3 <rafaelscherer3@gmail.com>
2026-01-24 03:37:24 -0300

    Add files via upload

--- pfcf 12h.py
+++ pfcf 12h.py
@@ -50,16 +50,69 @@
 #else:
 #    print ('negativo')
 #print ('par' if num % 2 == 0 else 'impar')
-nome = input('nome: ')
+#nome = input('nome: ')
 #resultado = len(nome)
 #if resultado > 5:
 #    print('2avfjdknvjkd')
 #else:
 #    print (resultado)
-resultado = nome.find('a')
+#resultado = nome.find('a')
 #print ('njfdnvhjfnbhjfg' if resultado > 5 else 'menor')
 
-print (resultado)
 
+#telefone= input ('telefone: ')
+#telefone = telefone.replace('-', '')
+#print (telefone)
 
- 
+#usuario = input ('usuario: ')
+
+#if len(usuario) > 12:
+#if caracteres < 12:
+#      print ('muito grande')
+
+#elif not usuario.find (' ') == -1:
+#    print ('tem espaço')
+
+#elif not usuario.find ('1', '2', '3', '4', '5', '6', '7', '8', '9', '0') == -1:
+#elif not usuario.isalpha():
+#     print ('tem numero')
+
+#else:
+    
+#
+#     print ('ok')
+#cartao = '123456789012345'
+#print (cartao[ :10:2])
+#valor_1 = 12345.6789
+#print (f" o valor 1 é {valor_1:.3f} ")
+#nome = input ('insura o seu nome')
+#while nome == '':
+#    print ('nome vazio')
+#    nome = input ('insura o seu nome')
+#print (f' seu nome é {nome}')
+#numero = int (input ('insura um numero de 0-10: '))
+#while numero < 0 or numero > 10:
+#   print ('numero invalido')
+#    numero = int (input ('insura um numero de 1-10 cazzo '))
+#print (f' seu numero é {numero}') 
+#inic = float (input ('insira o valor inicial: '))
+#juros = float (input ('insura a taxa de juros: '))
+#mes = int (input ('insura o numero de meses: '))
+
+#while inic == 0: 
+#    print ('valor inicial invalido')
+#    inic = float (input ('insura o valor inicial: '))
+
+#fim = inic * (1+ juros/100) ** mes
+#print (f' (o valor final é {fim:.2f} dinheiros.)')
+#import time 
+#t=10
+#while t > 0:
+#    print (t)
+#    time.sleep (1)
+#    t = t-1
+
+#print ('fim')
+for abc in range (3):
+    for def in range (1-9):
+    print (abc)
